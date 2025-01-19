199be4cf3e9996156f6a80e30e83f605467ad686
onumasatoru <onumasatoru@localhost>
2025-01-19 18:32:33 +0900

    マッピング重複修正。web.xmlを削除して@WebServletを採用

--- example/src/main/java/servlet/UranaiServlet.java
+++ example/src/main/java/servlet/UranaiServlet.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class UranaiServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 	//運勢をランダムで表示
